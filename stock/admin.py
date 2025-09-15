422f2d4f2c7a6932d28d1f4e3d37dc12b659a324
Lucas_Nog <lucasemanuelndq@gmail.com>
2025-09-15 20:31:14 -0300

    admin e mock

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import User, Admin, Product, Message, Order
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", "username", "cpf", "address", "birthDate", "email", "password")
+    search_fields = ("name", "username", "cpf", "address", "birthDate", "email", "password")
+
+@admin.register(Admin)
+class adminAdmin(admin.ModelAdmin):
+    list_display = ("name", "username", "cpf", "address", "birthDate", "email", "password", "adminLevel")
+    search_fields = ("name", "username", "cpf", "address", "birthDate", "email", "password", "adminLevel")
+
+
+@admin.register(Product)
+class productAdmin(admin.ModelAdmin):
+    list_display = ("name", "quantity", "allotment", "dueDate", "salePrice", "productionPrice", "description", "image")
+    search_fields = ("name", "quantity", "allotment", "dueDate", "salePrice", "productionPrice", "description", "image")
+
+@admin.register(Message)
+class messageAdmin(admin.ModelAdmin):
+    list_display = ("user", "description")
+    search_fields = ("user", "description")
+
+@admin.register(Order)
+class orderAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "orderDate", "status")
+    search_fields = ("user", "product", "orderDate", "status")
--- stock/models.py
+++ stock/models.py
@@ -11,25 +11,40 @@ class User(models.Model):
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Admin(User):
     adminLevel = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+    
 class Product(models.Model):
     name = models.CharField(max_length=50)
     quantity = models.IntegerField()
     allotment = models.IntegerField()
     dueDate = models.DateField()
-    priceSale = models.FloatField()
-    productionSale = models.FloatField()
+    salePrice = models.FloatField()
+    productionPrice = models.FloatField()
     description = models.TextField()
     image = models.ImageField(upload_to=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Message(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     description = models.TextField()
+
+    def __str__(self):
+        return str(self.user) + self.description
 
 class Order(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     orderDate = models.DateField()
     status = models.CharField(max_length=50)
+
+    def __str(self):
+        return self.user + self.product
